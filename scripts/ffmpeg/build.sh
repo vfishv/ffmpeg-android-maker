fe062fb9cad7c651876633fb6c81cb5f85dc2075
vFISHv <vfishv@gmail.com>
2022-01-11 00:44:55 +0800

    so
    ./ffmpeg-android-maker.sh  -abis=armeabi-v7a,arm64-v8a,x86,x86_64 --enable-libx264 --enable-libmp3lame --enable-libfreetype
    
    ./ffmpeg-android-maker.sh  -abis=armeabi-v7a,arm64-v8a,x86,x86_64 --enable-libx264 -android=16

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -41,8 +41,8 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --strip=${FAM_STRIP} \
   --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
   --extra-ldflags="$DEP_LD_FLAGS" \
-  --enable-shared \
-  --disable-static \
+  --disable-asm \
+  --disable-doc \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
   $ADDITIONAL_COMPONENTS || exit 1
