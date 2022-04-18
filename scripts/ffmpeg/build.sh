153cf738e54e4e0e0df4e409caf9a8675f45e8b4
zhangqt <zhangqt@akulaku.com>
2022-04-18 10:50:05 +0800

    Update build.sh

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -42,7 +42,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --extra-cflags="-O3 -fPIC $DEP_CFLAGS" \
   --extra-ldflags="$DEP_LD_FLAGS" \
   --enable-shared \
-  --disable-static \
+  --enable-static \
   --enable-small \
   --disable-ffplay \
   --disable-ffprobe \
