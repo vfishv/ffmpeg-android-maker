6d7eae0cd8f72ba468b72bedd160d2bf6ae86917
vfishv <vfishv@gmail.com>
2025-06-10 02:15:15 +0800

    libx264

--- scripts/ffmpeg/build.sh
+++ scripts/ffmpeg/build.sh
@@ -44,6 +44,7 @@ DEP_LD_FLAGS="-L${BUILD_DIR_EXTERNAL}/${ANDROID_ABI}/lib $FFMPEG_EXTRA_LD_FLAGS"
   --enable-shared \
   --enable-postproc \
   --enable-gpl \
+  --enable-encoder=libx264 \
   --disable-static \
   --pkg-config=${PKG_CONFIG_EXECUTABLE} \
   ${EXTRA_BUILD_CONFIGURATION_FLAGS} \
